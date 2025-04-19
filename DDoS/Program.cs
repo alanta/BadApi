37774d292c538c29cebc5eb131b70a124bb332d2
Marnix van Valen <marnix@alanta.nl>
2025-04-19 14:41:00 +0200

    DDos Demo

--- DDoS/Program.cs
+++ DDoS/Program.cs
@@ -1,12 +1,11 @@
-﻿using System.Net.Http.Json;
-using NBomber.CSharp;
+﻿using NBomber.CSharp;
 using NBomber.Http;
 
 using var httpClient = new HttpClient();
 
 var getScenario = Scenario.Create("ddos", async context =>
     {
-        var response = await httpClient.PostAsync("https://localhost:7280/ddos/good?id=1", null);
+        var response = await httpClient.PostAsync("https://localhost:7280/ddos/bad?id=1", null);
 
         return response.IsSuccessStatusCode ? Response.Ok() : Response.Fail();
     })
@@ -14,7 +13,7 @@ var getScenario = Scenario.Create("ddos", async context =>
     .WithLoadSimulations(
         Simulation.Inject(rate: 100,
             interval: TimeSpan.FromSeconds(1),
-            during: TimeSpan.FromSeconds(30))
+            during: TimeSpan.FromSeconds(300))
     );
 
 NBomberRunner
